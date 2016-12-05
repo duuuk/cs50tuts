daf361add59df4c9184e15c93fa428dac78aad33
duuuk <en.dulguun@gmail.com>
2016-12-06 02:10:21 +0800

    Prompt the user and convert the string to uppercase

--- initials.c
+++ initials.c
@@ -1,3 +1,20 @@
-#include "cs50.h"
+#include "cs50.c"
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+int main(void)
+
+  {
+    printf ("What is your name? ");
+    string name = GetString();
+
+    for (int i = 0, n = strlen(name); i < n; i++)
+      {
+        if (isalpha(name[i]))
+          {
+            printf ("%c", toupper(name[i]));
+          }
+      }
+    printf ("\n");
+  }
