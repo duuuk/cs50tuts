38061b7ffa84067c35efdbec73a779f0ad4f5351
duuuk <en.dulguun@gmail.com>
2016-12-10 17:59:15 +0800

    Initials done

--- initials.c
+++ initials.c
@@ -6,27 +6,33 @@
 int main(void)
 
   {
-    printf ("Word to Initialize ");
+    printf ("Word to Initialize:\n");
     string name = GetString();
 
-    for (int i = 0, n = strlen(name); i < n; i++)
+    if (isalpha(name[0]))
       {
-        if (isalpha(name[i]))
-          {
-            printf ("%c", toupper(name[i]));
-          }
-        else if (isspace(name[i]))
-          {
-            printf (" ");
-          }
-
-          
-
-        else
-          {
-            printf ("Please provide only words\n");
-            return 1;
-          }
+        printf ("%c", toupper(name[0]));
       }
+
+    else
+      {
+        printf ("Provide words only\n");
+        return 1;
+      }
+
+      for (int i = 0, n = strlen(name); i < n; i++)
+        {
+          if (isspace(name[i]))
+          {
+            printf ("%c", toupper(name[i+1]));
+            /*
+            else if (name[i+1])
+              {
+                printf ("Provide words only\n");
+                return 1;
+              }
+            */
+          }
+        }
     printf ("\n");
   }
