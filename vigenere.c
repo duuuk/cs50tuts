e729e89a5719d4639df7d7ad082b08840dd05c06
duuuk <en.dulguun@gmail.com>
2016-12-11 02:30:15 +0800

    no idea how

--- vigenere.c
+++ vigenere.c
@@ -3,44 +3,52 @@
 #include <string.h>
 #include <ctype.h>
 
-
-
 int main (int argc, string argv[])
 
-{
-  if (argc != 2)
+  {
+    string k = argv[1];
+    int klen = strlen(k);
     {
-      printf ("Please provide a single argument which is a key\n");
-      return 1;
-    }
-
-  string k = argv[1];
-  string p;
-
-  printf ("What string do you wish to encrypt?\n");
-  p = GetString();
-
-  /*for (int i = 0, n = strlen(k); i < n; i++) //Check
-    {
-      if (isalpha(k[i]))
+      if (argc == 2)
         {
-          printf ("What string do you wish to encrypt?\n");
-          p = GetString();
+          for (int i = 0; i < klen; i++)
+            {
+              if (!isalpha(k[i]))
+                {
+                  printf ("Key must be only alphabetical letters\n");
+                  return 1;
+                }
+            }
         }
       else
         {
-          printf ("Key must be only alphabetical characters\n");
+          printf ("Please provide a single argument which is a key.\nUsage: ./vigenere <key> (Without the brackets)\n");
           return 1;
         }
     }
-*/
-    for (int j = 0, m = strlen(p); j < m; j++)
+
+    printf ("Enter the text you wish to encrypt:\n");
+    string p = GetString();
+
+    for (int i = 0, j = 0, n = strlen(p); i < n; i++)
       {
-        printf ("%c", ((p[j] - 'a' + (k[j] % strlen(k)) % 26 + 'a')));
+        int lekey = toupper(k[j % klen]) - 'A';
+
+        if (isupper(p[i]))
+          {
+            printf ("%c", ((p[i] - 'A' + lekey) % 26 + 'A'));
+            j++;
+          }
+        else if (islower(p[i]))
+          {
+            printf ("%c", ((p[i] - 'a' + lekey) % 26 + 'a'));
+            j++;
+          }
+        else
+          {
+            printf ("%c", p[i]);
+          }
       }
-      //printf ("%c", ((m[i] - 'A' + k) % 26 + 'A'));
-
-
     printf ("\n");
 
-}
+  }
